7f6ba2898c26095544814b95dce3a6418d8c0e1a
mwendamwenda <benmwenda83@gmail.com>
2023-03-06 16:04:27 -0800

    guees my number app

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,10 +1,53 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number!';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+
+let  score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);//we can store this into a variable so that when we click the input number will be retrieved
+    console.log(guess,typeof guess);
+    
+    if(!guess) {
+        document.querySelector('.message').textContent = 'No Number!';
+
+    } else if(guess === secretNumber){
+        document.querySelector('.message').textContent = 'Correct Number!';
+
+    } else if(guess > secretNumber){
+        if(score > 1){
+            document.querySelector('.message').textContent = 'Too high!';
+      score-- ;//which is equal to  score = score - 1
+      document.querySelector('.score').textContent = score;
+
+        }else{
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
+        
+    }  else if(guess < secretNumber){
+        if(score > 1){
+            document.querySelector('.message').textContent = 'Too high!';
+      score-- ;//which is equal to  score = score - 1
+      document.querySelector('.score').textContent = score;
+
+        }else{
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
+        
+
+    }
+})
