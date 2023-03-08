939d7b80ab9d2712fd8e52a1a6dafb8f14285dfc
mwendamwenda <benmwenda83@gmail.com>
2023-03-08 14:50:55 -0800

    working with classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,5 +6,28 @@ const btnClosedModal = document.querySelector('.closed-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 console.log(btnShowModal);
 
-for (let i = 0; i < btnShowModal.length; i++);
-    console.log(btnShowModal(i).textContent);
+
+
+    const   openModel =  function(){
+        console.log('Button Clicked');
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+}
+
+const closedModel = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+
+
+
+for (let i = 0; i < btnShowModal.length; i++)
+    btnShowModal[i].addEventListener
+        ('click', openModel);
+
+    
+
+    btnClosedModal.addEventListener('click', closedModel);
+
+    overlay.addEventListener('click', closedModel); 
